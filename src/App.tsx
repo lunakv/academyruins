f758ba0b5a81deffe3912b80e6feeb858f1dec95
Vaclav Lunak <v.lunak@gmail.com>
2022-08-19 11:14:19 +0200

    redirect landing page to cr diff for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ArchivesPage from "./pages/ArchivesPage";
 import ErrorPage from "./pages/ErrorPage";
@@ -19,12 +18,12 @@ function App() {
       <Header onClick={() => setError(false)} />
       <ErrorBoundary hasError={hasError} onError={() => setError(true)}>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/archives" element={<ArchivesPage />} />
           <Route path="/diff/cr/:codes" element={<RulesDiffPage />} />
           <Route path="/preview" element={<PreviewPage />} />
           <Route path="/debug" element={<DiffDebugPage />} />
+          <Route path="/" element={<Navigate to="/diff/cr/latest" />} />
           <Route path="*" element={<ErrorPage message={"This page does not exist."} />} />
           {/* TODO doc diff routes */}
         </Routes>
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,6 +5,12 @@ const AboutPage = () => (
   <Container fluid="lg" className="offset">
     <h2 className="text-center">About</h2>
     <p>
+      Academy Ruins is a Magic: the Gathering knowledge portal. Its primary purpose is hosting diffs of the various
+      Magic rules documents, specifically the Comprehensive Rules, the Magic Tournament Rules, and the Infraction
+      Procedure Guide. It also houses an API allowing programmatic access to those documents, as well as an archive
+      containing many of their old versions.
+    </p>
+    <p>
       This site is maintained by <b>Václav Luňák</b>, a RA from EU–Central. The CR diffs were a project first created by{" "}
       <a href="https://yawgatog.com/">Yawgatog</a> and later continued by Venser’s Journal. <i>Academy Ruins</i> is a
       continuation of those efforts.
@@ -15,7 +21,7 @@ const AboutPage = () => (
     </p>
 
     <p>
-      Otherwise—for suggestions, more general feedback, etc.— feel free to reach out on Twitter{" "}
+      Otherwise—for suggestions, more general feedback, etc.—feel free to reach out on Twitter{" "}
       <a href="https://twitter.com/just_vaasa">@just_vaasa</a>, IRC (vaasa on Libera.Chat), or through the Judge Academy
       Discord.
     </p>
