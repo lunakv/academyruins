950da2d40bbb1e77f1c6617cf831c08dddd99a21
Vaclav Lunak <v.lunak@gmail.com>
2022-09-29 17:50:54 +0200

    adjusted mobile styling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,39 +13,39 @@ interface IconProps {
 }
 const LandingPageIcon = ({ icon, description }: IconProps) => (
   <Row>
-    <Col xs={3} lg={12}>
+    <Col xs={3} md={12}>
       {icon}
     </Col>
-    <Col xs={9} lg={12} className="mt-lg-4 icon-desc">
+    <Col xs={9} md={12} className="mt-md-4 icon-desc">
       {description}
     </Col>
   </Row>
 );
 
 const LandingPage = () => (
-  <Row id="landing-wrapper" className="bg-dark text-cream text-lg-center">
+  <Row id="landing-wrapper" className="bg-dark text-cream text-md-center">
     <Col xs={12} id="landing-title" className="cursive text-center">
       Academy Ruins
     </Col>
     <Col xs={12}>
       <hr className="w-75 mx-auto" />
     </Col>
-    <Col lg={3}>
+    <Col md={3}>
       <Link to="/diff/cr">
         <LandingPageIcon icon={<DiffIcon />} description="Diffs" />
       </Link>
     </Col>
-    <Col lg={3}>
+    <Col md={3}>
       <Link to="/archives">
         <LandingPageIcon icon={<ArchiveIcon />} description="Archives" />
       </Link>
     </Col>
-    <Col lg={3}>
+    <Col md={3}>
       <a href={process.env.REACT_APP_API_URL + "/docs"}>
         <LandingPageIcon icon={<GearIcon />} description="API" />
       </a>
     </Col>
-    <Col lg={3}>
+    <Col md={3}>
       <Link to="/about">
         <LandingPageIcon icon={<QuestionIcon />} description="About" />
       </Link>
