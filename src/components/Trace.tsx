33de60bc8eba279f53efb95280cc83298229f0eb
Václav Luňák <v.lunak@gmail.com>
2025-02-16 02:37:16 +0100

    Add fuzzy trace support

--- src/components/Trace.tsx
+++ src/components/Trace.tsx
@@ -1,19 +1,32 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { ReactComponent as ExclamationIcon } from "bootstrap-icons/icons/exclamation-circle.svg";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 import VerticalLine from "./VerticalLine";
 import TraceSegment, { TraceItem } from "./TraceSegment";
 
+export interface TraceModel {
+  ruleNumber: string;
+  items: TraceItem[];
+}
+
 interface Props {
-  trace: TraceItem[] | null;
+  trace: TraceModel | null;
   ruleNumber: string;
 }
 
 const Trace = ({ trace, ruleNumber }: Props) => {
+  const actualRuleNumber = trace?.ruleNumber;
+
   return (
     <Container fluid="lg" className="my-5">
-      <Row>
+      <Row className="justify-content-center">
         <Col xs={12} className="text-center mb-3">
-          <h2>Trace of rule {ruleNumber}</h2>
+          <h2>Trace of rule {actualRuleNumber}</h2>
         </Col>
+        {ruleNumber !== actualRuleNumber && (
+          <Col md={8}>
+            <TraceRedirectionMessage requested={ruleNumber} actual={actualRuleNumber!} />
+          </Col>
+        )}
       </Row>
 
       <TraceContent trace={trace} />
@@ -29,7 +42,7 @@ const TraceContent = ({ trace }: Pick<Props, "trace">) => {
       </div>
     );
   }
-  if (trace.length === 0) {
+  if (trace.items.length === 0) {
     return (
       <div className="text-center">
         <i>No changes found for this rule.</i>
@@ -39,12 +52,12 @@ const TraceContent = ({ trace }: Pick<Props, "trace">) => {
 
   return (
     <>
-      {trace.map((item, i) => (
+      {trace.items.map((item, i) => (
         <>
           <Row className="justify-content-center">
             <TraceSegment item={item} />
           </Row>
-          {i < trace.length - 1 && (
+          {i < trace.items.length - 1 && (
             <Row className="justify-content-center">
               <VerticalLine height="7em" />
             </Row>
@@ -52,6 +65,22 @@ const TraceContent = ({ trace }: Pick<Props, "trace">) => {
         </>
       ))}
     </>
+  );
+};
+
+const TraceRedirectionMessage = ({ requested, actual }: { requested: string; actual: string }) => {
+  return (
+    <Alert variant="warning">
+      <Row>
+        <Col xs="auto" className="m-auto">
+          <ExclamationIcon width={20} height={20} />
+        </Col>
+        <Col>
+          Rule {requested} contains just a keyword name. The trace was automatically redirected to rule {actual}, which
+          contains its definition. <a href="?exact=true">Disable redirection.</a>
+        </Col>
+      </Row>
+    </Alert>
   );
 };
 
--- src/pages/TracePage.tsx
+++ src/pages/TracePage.tsx
@@ -1,11 +1,12 @@
-import Trace from "../components/Trace";
+import Trace, { TraceModel } from "../components/Trace";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { TraceItem } from "../components/TraceSegment";
+import { useParams, useSearchParams } from "react-router-dom";
 import Loading from "../components/Loading";
 
-async function fetchTrace(ruleNumber: string): Promise<TraceItem[] | null> {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/cr/trace/${encodeURIComponent(ruleNumber)}`);
+async function fetchTrace(ruleNumber: string, exact: boolean): Promise<TraceModel | null> {
+  const res = await fetch(
+    `${process.env.REACT_APP_API_URL}/cr/trace/${encodeURIComponent(ruleNumber)}?find_definition=${!exact}`
+  );
   if (res.status === 404) return null;
   if (res.status === 429) throw new Error("Rate limit exceeded.");
   return res.json();
@@ -13,16 +14,18 @@ async function fetchTrace(ruleNumber: string): Promise<TraceItem[] | null> {
 
 const TracePage = () => {
   const params = useParams();
+  const [queryParams] = useSearchParams();
   const [isLoading, setLoading] = useState(true);
-  const [trace, setTrace] = useState<TraceItem[] | null>(null);
+  const [trace, setTrace] = useState<TraceModel | null>(null);
   const ruleNumber = params.rule!;
+  const exactSearch = queryParams.get("exact");
 
   const [error, setError] = useState(undefined);
   if (error) throw error;
 
   useEffect(() => {
     setLoading(true);
-    fetchTrace(ruleNumber)
+    fetchTrace(ruleNumber, exactSearch === "true")
       .then((trace) => {
         setLoading(false);
         setTrace(trace);
@@ -30,7 +33,7 @@ const TracePage = () => {
       .catch((e) => {
         setError(e);
       });
-  }, [ruleNumber]);
+  }, [ruleNumber, exactSearch]);
 
   return (
     <Loading isLoading={isLoading} className="mt-5">
